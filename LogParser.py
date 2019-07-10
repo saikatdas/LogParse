e1653bdc3c38e772a82b6239c78b230b7307a4e6
saikatdas <dassaikat93@yahoo.com>
2019-07-10 16:44:21 +0530

    modified code

--- LogParser.py
+++ LogParser.py
@@ -1,12 +1,16 @@
 import sys,os
+len=sys.argv
+if(len==0):
+    print ("Kindly enter anytext to search")
+    sys.exit(0)
 strKeyWord=str(sys.argv[1])
 print(strKeyWord)
 #select all log files present in current working directory
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 for f in files:
     programfilename= str(f)
-    isPythonExtension=programfilename.endswith('.py')
-    if(not isPythonExtension): #we don't want to open python file 
+    isPythonExtension=programfilename.endswith('.txt')
+    if(isPythonExtension): #we don't want to open python file or screenshot , just open txt file
         print("===============Opening================")
         print(f)
         print("--------------------------------------")
